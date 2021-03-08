676c2ee23f2553f6395cdac114d8a85a1b1406d7
Philipp Hadjimina <p.m.hadjimina@gmail.com>
2021-03-08 21:47:33 +0100

    fixed key

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { faExclamation } from "@fortawesome/free-solid-svg-icons";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
-import * as Credentials from "./credentials.js";
+/*import * as Credentials from "./credentials.js";*/
 import * as Strings from "./strings.js"
 
 const getWidth = () =>
